165c796fc260c668da163b7a84dc03f00dfc5390
Tetsuro Kurita <tkurita@mac.com>
2008-10-23 14:30:55 +0000

    [TeXBridge] add FindRoughlyCommand.m
    git-svn-id: file:///Users/tkurita/Dev/svnrepos/TeXBridge/trunk@2199 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ FindRoughlyCommand.m
@@ -0,0 +1,37 @@
+#import "FindRoughlyCommand.h"
+#import "miClient.h"
+
+#define useLog 0
+@implementation FindRoughlyCommand
+- (id)performDefaultImplementation
+{
+#if useLog
+	NSLog(@"FindRoughlyCommand");
+	NSLog([[self arguments] description]);
+	NSLog([[self directParameter] description]);
+	NSLog([[self evaluatedArguments] description]);
+	NSLog([[self evaluatedReceivers] description]);
+	NSLog(@"with source : %@", [[self arguments] objectForKey:@"withSource"]);
+#endif	
+	NSString *dvi_path = [[self arguments] objectForKey:@"inDvi"];
+	NSString *source_name = [[self arguments] objectForKey:@"withSource"];
+	NSNumber *start_pos = [[self arguments] objectForKey:@"startLine"];
+	
+	NSString *tex_path;
+	if (source_name) {
+		tex_path = [[dvi_path stringByDeletingLastPathComponent] stringByAppendingPathComponent:source_name];
+	} else {
+		tex_path = [[dvi_path stringByDeletingPathExtension] stringByAppendingPathExtension:@"tex"];
+	}
+	
+	FSRef ref;
+	OSStatus status = FSPathMakeRef((UInt8 *)[tex_path fileSystemRepresentation], &ref, NULL);
+	if (status == noErr) {
+		id miclient = [[[miClient alloc] init] autorelease];
+		BOOL is_success = [miclient jumpToFile:&ref paragraph:start_pos];
+	}
+	
+	return [super performDefaultImplementation];
+}
+
+@end
