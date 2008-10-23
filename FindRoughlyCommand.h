b90dff8ab032f8927bc9bba4afc58b279884f833
Tetsuro Kurita <tkurita@mac.com>
2008-10-23 14:32:31 +0000

    [TeXBridge] add FindRoughlyCommand.h
    git-svn-id: file:///Users/tkurita/Dev/svnrepos/TeXBridge/trunk@2200 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ FindRoughlyCommand.h
@@ -0,0 +1,16 @@
+//
+//  FindRoughlyCommand.h
+//  TryScriptable2
+//
+//  Created by ŒI“c “N˜Y on 08/10/23.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface FindRoughlyCommand : NSScriptCommand {
+
+}
+
+@end
